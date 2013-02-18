112a5dbf8f3e49befec67bc99fbc17952735b3e2
Stephen Moore <stephenmoore56@msn.com>
2013-02-17 21:44:30 -0600

    Changed connection string after opening firewall on RDS

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,13 @@ exports.index = function(req, res){
 	res.render('index', { title: 'Node.js' });
 };
 exports.mysqltest = function(req, res){
-	var express	   = require('express');
 	var mysql      = require('mysql');
-	var app		   = express();
-	if (app.settings.env === 'development') {
-		var connection = mysql.createConnection({
-		  host     : 'localhost',
-		  user     : 'root',
-		  password : 'gsnyder56',
-		  database : 'birding'
-		});		
-	} else {
-		// production system
-		var connection = mysql.createConnection({
-		  host     : 'mooredatabase.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
-		  user     : 'smoore',
-		  password : 'gsnyder56',
-		  database : 'birding'
-		});				
-	}
+	var connection = mysql.createConnection({
+	  host     : 'mooredatabase.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
+	  user     : 'smoore',
+	  password : 'gsnyder56',
+	  database : 'birding'
+	});		
 	connection.connect();
 	connection.query('SELECT COUNT(*) AS speciesCount FROM aou_list;', function(err, rows, fields) {
 		if (err) throw err;
