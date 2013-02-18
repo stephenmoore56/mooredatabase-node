deccb6bbc12633d9ae27bc91a0d5c6012f50767e
Stephen Moore <stephenmoore56@msn.com>
2013-02-17 21:25:55 -0600

    Trying new connection string

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,7 @@ exports.mysqltest = function(req, res){
 		var connection = mysql.createConnection({
 		  host     : 'mooredatabase.cd02yxrr7fxm.us-east-1.rds.amazonaws.com',
 		  user     : 'smoore',
-		  password : 'gsnyder56',
-		  database : 'birding'
+		  password : 'gsnyder56'
 		});				
 	}
 	connection.connect();
