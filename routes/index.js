1af83bdf6be71a9806fe6bd56cb733118f74fa22
Stephen Moore <stephenmoore56@msn.com>
2013-02-17 20:56:45 -0600

    Testing db connection

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,6 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Node.js' });
 };
+exports.mysqltest = function(req, res){
+  res.render('mysqltest', { title: 'MySQL Test' });
+};
