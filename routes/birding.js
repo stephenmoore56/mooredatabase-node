135883c8fe5c6abf186863ffc9446224c7d6e105
Stephen Moore <stephenmoore56@msn.com>
2013-02-21 22:16:57 -0600

    Added controllers, routing, arranged views in folders

---
+++ routes/birding.js
@@ -0,0 +1,29 @@
+// birding controller
+exports.orders = function(req, res){
+	var mysqlDatabase = require('../modules/mysqlDatabase.js');
+	var connection = mysqlDatabase.connect();		
+	connection.connect();	
+	var	sql = "SELECT \
+			  aou_order.order_name, \
+			  aou_order.notes AS order_notes, \
+			  ( SELECT COUNT(*) \
+                FROM \
+                aou_list aol2 \
+                WHERE \
+                aol2.order = aou_order.order_name) AS totalSpecies, \
+              COUNT(DISTINCT aou_list.id) AS speciesCount \
+              FROM \
+			  sighting \
+			  INNER JOIN aou_list ON sighting.aou_list_id = aou_list.id \
+			  INNER JOIN aou_order ON aou_list.order = aou_order.order_name \
+			  GROUP BY aou_order.order_name, aou_order.notes \
+			  ORDER BY COUNT(DISTINCT aou_list.id) DESC";
+	connection.query(sql, function(err, rows) {
+		if (err) {
+			res.render('error', { title: 'Database Error', description: 'A database error occurred: ' + err.message });
+		} else {
+			res.render('birding/orders', { title: 'Species and Sightings By Order', orders: rows });			
+		}
+	});
+	connection.end();
+};
