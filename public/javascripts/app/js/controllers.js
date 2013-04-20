ab972eb2c04cc42dc90bed281005b63bf5af7d78
Stephen Moore <stephenmoore56@msn.com>
2013-04-19 19:55:17 -0500

    Added angularjs sorting to tables

--- public/javascripts/app/js/controllers.js
+++ public/javascripts/app/js/controllers.js
@@ -2,9 +2,31 @@ OrdersCtrl = function($scope, $http) {
 	$http.get('/birding/ordersjson').success(function(data) {
 		$scope.orders = data;
 	});
+	
+	$scope.orderProp = 'speciesCount';
+	$scope.reverse = true;
+	
+	$scope.setOrderProp = function(newOrderProp) {
+		if (newOrderProp == $scope.orderProp) {
+			$scope.reverse = ! $scope.reverse;
+		} else {
+   			$scope.orderProp = newOrderProp;			
+		}
+  	}		
 }
 MonthsCtrl = function($scope, $http) {
 	$http.get('/birding/monthsjson').success(function(data) {
 		$scope.months = data;
 	});
+	
+	$scope.orderProp = 'monthNumber';
+	$scope.reverse = false;
+	
+	$scope.setOrderProp = function(newOrderProp) {
+		if (newOrderProp == $scope.orderProp) {
+			$scope.reverse = ! $scope.reverse;
+		} else {
+   			$scope.orderProp = newOrderProp;			
+		}
+  	}	
 }
