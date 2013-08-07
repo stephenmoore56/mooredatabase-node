51f2be2497d2804c4770fe68515e1b057539f052
Stephen Moore <stephenmoore56@msn.com>
2013-08-06 21:06:15 -0500

    Static content SPA added

--- public/javascripts/app/js/app.js
+++ public/javascripts/app/js/app.js
@@ -1,3 +1,20 @@
 'use strict';
+// create app
+var myApp = angular.module('myApp', ['ngResource','myAppServices']);
 
-angular.module('myApp', ['ngResource','myAppServices']);
+// routing for static pages (SPA)
+myApp.config(function($routeProvider){
+	$routeProvider
+		.when('',
+		{
+			templateUrl: '/partials/backend.html'
+		})
+		.when('/backend',
+		{
+			templateUrl: '/partials/backend.html'
+		})		
+		.when('/frontend',
+		{
+			templateUrl: '/partials/frontend.html'
+		});
+});
