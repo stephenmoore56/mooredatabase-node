457b877302e0dd52678b230d6b81f84a6b8908ed
Stephen Moore <stephenmoore56@msn.com>
2013-08-21 22:11:14 -0500

    JS/CSS cleanup

--- public/javascripts/uglifyjs.sh
+++ public/javascripts/uglifyjs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uglifyjs jquery.colorbox-min.js mooredatabase.js slideshow.js modernizr-2.6.2.min.js -c -o mooredatabase.min.js
+uglifyjs mooredatabase.js slideshow.js modernizr-2.6.2.min.js -c -o mooredatabase.min.js
 uglifyjs google_chart_species_by_order.js -c -o google_chart_species_by_order.min.js
 uglifyjs google_chart_species_by_month.js -c -o google_chart_species_by_month.min.js
 uglifyjs jquery.cycle.lite.js -c -o jquery.cycle.lite.min.js
