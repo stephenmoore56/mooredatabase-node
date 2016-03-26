fc72330e26bb38fb026e55b42ded4ae78b2a4c92
Stephen Moore <stephenmoore56@msn.com>
2016-03-26 10:54:02 -0500

    Fixing bootstrap elements

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 (function () {
     'use strict';
+
+    /* express */
     let express = require('express');
     let app = express();
+    let routes = require('./lib/routes')(app, express);
+
+    /* ejs templating */
     let engine = require('ejs-locals');
     let ejs = require('ejs');
     app.engine('ejs', engine);
-    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
-    let routes = require('./lib/routes')(app, express);
+    
+    /* start app */
+    app.set('port', process.env.PORT || 3000);
     app.listen(app.get('port'), () => {
         console.log("Express server listening on port " + app.get('port') + " in " + process.env.NODE_ENV + " environment");
     });
