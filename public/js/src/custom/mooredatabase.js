0d74896054c0f6fc62bbba43c0ca0e2303086478
Stephen Moore <stephenmoore56@msn.com>
2016-05-05 11:22:57 -0500

    Working on back button glitch

--- public/js/src/custom/mooredatabase.js
+++ public/js/src/custom/mooredatabase.js
@@ -1,33 +1,18 @@
-(function() {
-    'use strict';
-    $(function() {
-        // start up carousel and slideshow
-        $("#photoCarousel")
-            .carousel({
-                interval: 5000
-            })
-            .show();
-        $("#slideshow")
-            .show()
-            .cycle();
-        // change active nav pill
-        $('#toolsNav a')
-            .on('click', function() {
-                $('#toolsNav li')
-                    .each(function() {
-                        $(this)
-                            .removeClass('active');
-                    });
+var mooredatabaseCustom = {
+    setNavPill: function() {
+        console.log('in function');
+        // first remove active class from pills
+        $('#toolsNav li')
+            .each(function() {
                 $(this)
-                    .parent()
-                    .addClass('active');
+                    .removeClass('active');
             });
         $('#toolsNav li')
             .each(function() {
                 $(this)
                     .removeClass('active');
             });
-        // figure out which pill should be made active
+        // set appropriate pill to active
         var myHash = window.location.hash.split("/");
         if ($.inArray("orders", myHash) !== -1) {
             $('#ordersNavPill')
@@ -61,5 +46,71 @@
             $('#bootstrapNavPill')
                 .addClass('active');
         }
+    }
+};
+(function() {
+    'use strict';
+    $(function() {
+        // start up carousel and slideshow
+        $("#photoCarousel")
+            .carousel({
+                interval: 5000
+            })
+            .show();
+        $("#slideshow")
+            .show()
+            .cycle();
+        // change active nav pill
+        mooredatabaseCustom.setNavPill();
+        // $('#toolsNav a')
+        //     .on('click', function() {
+        //         $('#toolsNav li')
+        //             .each(function() {
+        //                 $(this)
+        //                     .removeClass('active');
+        //             });
+        //         $(this)
+        //             .parent()
+        //             .addClass('active');
+        //     });
+        // $('#toolsNav li')
+        //     .each(function() {
+        //         $(this)
+        //             .removeClass('active');
+        //     });
+        // // figure out which pill should be made active
+        // var myHash = window.location.hash.split("/");
+        // if ($.inArray("orders", myHash) !== -1) {
+        //     $('#ordersNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("months", myHash) !== -1) {
+        //     $('#monthsNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("years", myHash) !== -1) {
+        //     $('#yearsNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("species", myHash) !== -1) {
+        //     $('#speciesNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("detail", myHash) !== -1) {
+        //     $('#speciesNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("nodejs", myHash) !== -1 || window.location.hash === "") {
+        //     $('#nodejsNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("angularjs", myHash) !== -1) {
+        //     $('#angularjsNavPill')
+        //         .addClass('active');
+        // }
+        // if ($.inArray("bootstrap", myHash) !== -1) {
+        //     $('#bootstrapNavPill')
+        //         .addClass('active');
+        // }
     });
 })();
