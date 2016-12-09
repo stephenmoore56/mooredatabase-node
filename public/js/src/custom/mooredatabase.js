892de5811e9adf2f48ce77773d494a5526ca3413
Stephen Moore <stephenmoore56@msn.com>
2016-12-08 22:22:29 -0600

    Fixing nav pill activation on tools page

--- public/js/src/custom/mooredatabase.js
+++ public/js/src/custom/mooredatabase.js
@@ -1,112 +1,69 @@
 var mooredatabaseCustom = {
-    setNavPill: function() {
-	'use strict';
+    setNavPill: function () {
+        'use strict';
         // first remove active class from pills
-        $('#toolsNav li')
-            .each(function() {
-                $(this)
-                    .removeClass('active');
-            });
-        $('#toolsNav li')
-            .each(function() {
-                $(this)
-                    .removeClass('active');
-            });
+        $('.nav li')
+        .each(function () {
+            $(this)
+            .removeClass('active');
+        });
         // set appropriate pill to active
         var myHash = window.location.hash.split("/");
         if ($.inArray("orders", myHash) !== -1) {
             $('#ordersNavPill')
-                .addClass('active');
+            .addClass('active');
         }
         if ($.inArray("months", myHash) !== -1) {
             $('#monthsNavPill')
-                .addClass('active');
+            .addClass('active');
         }
         if ($.inArray("years", myHash) !== -1) {
             $('#yearsNavPill')
-                .addClass('active');
+            .addClass('active');
+        }
+        if ($.inArray("species", myHash) !== -1) {
+            $('#speciesNavPill')
+            .addClass('active');
         }
         if ($.inArray("detail", myHash) !== -1) {
             $('#speciesNavPill')
-                .addClass('active');
+            .addClass('active');
         }
         if ($.inArray("nodejs", myHash) !== -1 || window.location.hash === "") {
             $('#nodejsNavPill')
-                .addClass('active');
+            .addClass('active');
         }
         if ($.inArray("angularjs", myHash) !== -1) {
             $('#angularjsNavPill')
-                .addClass('active');
+            .addClass('active');
         }
         if ($.inArray("bootstrap", myHash) !== -1) {
             $('#bootstrapNavPill')
-                .addClass('active');
+            .addClass('active');
         }
     }
 };
-(function() {
+(function () {
     'use strict';
-    $(function() {
+    $(function () {
         // start up carousel and slideshow
         $("#photoCarousel")
-            .carousel({
-                interval: 5000
-            })
-            .show();
+        .carousel({
+            interval: 5000
+        })
+        .show();
         $("#slideshow")
-            .show()
-            .cycle();
+        .show()
+        .cycle();
         // change active nav pill
         mooredatabaseCustom.setNavPill();
-        // $('#toolsNav a')
-        //     .on('click', function() {
-        //         $('#toolsNav li')
-        //             .each(function() {
-        //                 $(this)
-        //                     .removeClass('active');
-        //             });
-        //         $(this)
-        //             .parent()
-        //             .addClass('active');
-        //     });
-        // $('#toolsNav li')
-        //     .each(function() {
-        //         $(this)
-        //             .removeClass('active');
-        //     });
-        // // figure out which pill should be made active
-        // var myHash = window.location.hash.split("/");
-        // if ($.inArray("orders", myHash) !== -1) {
-        //     $('#ordersNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("months", myHash) !== -1) {
-        //     $('#monthsNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("years", myHash) !== -1) {
-        //     $('#yearsNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("species", myHash) !== -1) {
-        //     $('#speciesNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("detail", myHash) !== -1) {
-        //     $('#speciesNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("nodejs", myHash) !== -1 || window.location.hash === "") {
-        //     $('#nodejsNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("angularjs", myHash) !== -1) {
-        //     $('#angularjsNavPill')
-        //         .addClass('active');
-        // }
-        // if ($.inArray("bootstrap", myHash) !== -1) {
-        //     $('#bootstrapNavPill')
-        //         .addClass('active');
-        // }
+
+        // nav pills are different on tools page; no controller
+        $('.toolPill').on('click', function () {
+            $('.nav li').each(function () {
+                $(this).removeClass('active');
+            });
+            $(this).addClass('active');
+        });
     });
 })();
